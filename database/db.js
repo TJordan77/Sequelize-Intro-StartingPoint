566b662a24edfbec8d41e25bcdb75922dd1e1007
TJordan77 <tvon777@gmail.com>
2025-07-01 02:28:12 -0400

    Update db.js

--- database/db.js
+++ database/db.js
@@ -4,7 +4,14 @@ const pg = require("pg");
 const db = new Sequelize(
   process.env.DATABASE_URL,
   {
-    logging: false, // comment this line to enable logging
+    dialect: "postgres",
+    protocol: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      logging: false, // comment this line to enable logging
+      },
   }
 );
 
