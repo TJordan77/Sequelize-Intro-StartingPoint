b98dcf5f846b819cac47681624c54c51995b4274
TJordan77 <tvon777@gmail.com>
2025-07-01 02:35:09 -0400

    Update db.js

--- database/db.js
+++ database/db.js
@@ -1,21 +1,16 @@
 const { Sequelize } = require("sequelize");
 const pg = require("pg");
 
-const db = new Sequelize(
-  process.env.DATABASE_URL,
-  {
-    dialect: "postgres",
-    protocol: "postgres",
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false,
-      logging: false, // comment this line to enable logging
-      },
+const db = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "postgres",
+  protocol: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
     },
-  }
-);
-
+  },
+  logging: false,
+});
 
 module.exports = db;
-
